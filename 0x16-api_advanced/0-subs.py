539f3d656cc30f413f1d692dd79d28fbb923c9df
tamer_maarouf <tamermaarouf@gmail.com>
2024-08-24 14:29:17 +0300

    0x15. JavaScript - Web jQuery

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-
-"""Module for task 0"""
+"""
+Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribeson a given subreddit"""
-    import requests
-
-    # header={"User-Agent": "linux:0x16 api advanced v1"}
-
+    """
+    Return the total number of subscribeson a given subreddit
+    """
     response = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if response.status_code >= 300:
-        return 0
 
-    return response.json().get("data").get("subscribers")
+    if response.status_code == 300:
+        return response.json().get("data").get("subscribers")
+    else:
+        return 0
