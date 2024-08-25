83121cec35a26343c67b2e57f33c9a9eee3f91cf
tamer_maarouf <tamermaarouf@gmail.com>
2024-08-25 09:53:13 +0300

    Web stack debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes bad "phpp" extensions to "php" in "wp-settings.php"
+# Creat a manifest that fix all termintion of phpp.
 
-exec { 'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path    => '/usr/local/bin/:/bin/'
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
